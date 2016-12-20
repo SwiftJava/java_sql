d12fe7533c5fbddabb387b48f4c2bade8cdd0002
John Holdsworth <github@johnholdsworth.com>
2016-12-20 13:04:43 +0000

    Initial commit

---
+++ java_sql.h
@@ -0,0 +1,19 @@
+//
+//  java_sql.h
+//  java_sql
+//
+//  Created by John Holdsworth on 14/07/2016.
+//  Copyright (c) 2016 John Holdsworth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for java_sql.
+FOUNDATION_EXPORT double java_sqlVersionNumber;
+
+//! Project version string for java_sql.
+FOUNDATION_EXPORT const unsigned char java_sqlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <java_sql/PublicHeader.h>
+
+
